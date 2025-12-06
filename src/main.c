4cda9c952feb065460d34f523491a955e06070dc
anotherBL <tranbaolam006@gmail.com>
2025-12-06 21:47:55 +0700

    fix: tokenize the input

--- src/main.c
+++ src/main.c
@@ -12,10 +12,17 @@ int main(int argc, char *argv[]) {
     fgets(input, sizeof(input), stdin);
     // Remove trailing end-line and add null byte
     input[strcspn(input, "\n")] = '\0';
-    if (strcmp(input, "exit") == 0)
+
+    // Tokenize the input
+    char *saveptr1;
+    char *command_token;
+    char copy_input[512];
+    memcpy(copy_input, input, sizeof(input));
+    command_token = strtok_r(copy_input, " \t", &saveptr1);
+    if (strcmp(command_token, "exit") == 0)
       break;
-    else if (strncmp(input, "echo", 4) == 0) {
-      printf("%s\n", input + 5);
+    else if (strcmp(command_token, "echo") == 0) {
+      printf("%s\n", saveptr1);
     } else
       printf("%s: command not found\n", input);
   }
